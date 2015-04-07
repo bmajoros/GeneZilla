000d0d73d50cb0bc1ec9015663af82b9b231dbd4
Bill Majoros <martiandna@gmail.com>
2015-04-07 15:11:00 -0400

    update

--- project-all.pl
+++ project-all.pl
@@ -9,5 +9,10 @@ my ($dir)=@ARGV;
 my @files=`ls chunks`;
 my $n=@files;
 for(my $i=0 ; $i<$n ; ++$i) {
+  my $file=$files[$i];
+  chomp $file;
+  next unless $file=~/^(\d+)-alt.fasta/;
+  my $id=$1;
+  
 }
 
