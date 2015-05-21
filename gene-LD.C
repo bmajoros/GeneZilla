f0536b1f6907792ef2507067f13c7fe62e88d988
Bill Majoros <martiandna@gmail.com>
2015-05-21 17:40:06 -0400

    update

--- gene-LD.C
+++ gene-LD.C
@@ -8,17 +8,24 @@
 #include "BOOM/String.H"
 #include "BOOM/CommandLine.H"
 #include "BOOM/GCF.H"
+#include "BOOM/Map.H"
 using namespace std;
 using namespace BOOM;
+
+struct Individual {
+  int hap[2][2]; // [which chrom][which gene]
+};
 
 class Application {
 public:
   Application();
   int main(int argc,char *argv[]);
   void recode(GCF &,int whichGene);
-projected:
+protected:
   Map<String,int> haploMap[2]; // for two genes
   Vector<String> haplotypes[2]; // for two genes
+  Array1D<Individual> individuals;
+  String genotypeToString(const Array1D<int> &genotype);
 };
 
 
@@ -58,6 +65,7 @@ int Application::main(int argc,char *argv[])
   GCF gcf1(file1), gcf2(file2);
   const int numIndiv=gcf1.numIndividuals();
   if(gcf2.numIndividuals()!=numIndiv) throw "files have different numbers of individuals";
+  individuals.resize(numIndiv);
 
   // Re-code the haplotypes as integers (separately for each gene)
   recode(gcf1,0); recode(gcf2,1);
@@ -73,7 +81,35 @@ int Application::main(int argc,char *argv[])
 void Application::recode(GCF &gcf,int whichGene)
 {
   const int numIndiv=gcf.numIndividuals();
+  for(int i=0 ; i<numIndiv ; ++i) {
+    const GcfIndividual &indiv=gcf.getIthIndividual(i);
+    recode(indiv.chrom[0],whichGene);
+    recode(indiv.chrom[1],whichGene);
+  }
+}
+
+
+
+void Application::recode(const Array1D<int> &chrom,int whichGene)
+{
+  String s=genotypeToString(indiv.chrom[0]);
+  int hapID;
+  if(!haploMap[whichGene].isDefined(s)) {
+    hapID=haploMap[whichGene][s]=haplotypes[whichGene].size();
+    haplotypes[whichGene].push_back(s);
+  }
+  else hapID=haploMap[whichGene][s];
   
+}
+
+
+
+String Application::genotypeToString(const Array1D<int> &gt)
+{
+  const int n=gt.size();
+  char str[n+1]; str[n]='\0';
+  for(int i=0 ; i<n ; ++i) str[i]='0'+gt[i];
+  return str;
 }
 
 
